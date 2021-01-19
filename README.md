db335ce10f6d5fd771b83106150256a52f95402e
Brent Shields <bshields@cpan.org>
2021-01-19 14:49:36 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+LG::QuickMemo\_Plus::Extract::Memo - It's new $module
+
+# SYNOPSIS
+
+    use LG::QuickMemo_Plus::Extract::Memo;
+
+# DESCRIPTION
+
+LG::QuickMemo\_Plus::Extract::Memo is ...
+
+# LICENSE
+
+Copyright (C) Brent Shields.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Brent Shields <bshields@cpan.org>
---
+++ lib/LG/QuickMemo_Plus/Extract/Memo.pm
@@ -0,0 +1,39 @@
+package LG::QuickMemo_Plus::Extract::Memo;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+LG::QuickMemo_Plus::Extract::Memo - It's new $module
+
+=head1 SYNOPSIS
+
+    use LG::QuickMemo_Plus::Extract::Memo;
+
+=head1 DESCRIPTION
+
+LG::QuickMemo_Plus::Extract::Memo is ...
+
+=head1 LICENSE
+
+Copyright (C) Brent Shields.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Brent Shields E<lt>bshields@cpan.orgE<gt>
+
+=cut
+

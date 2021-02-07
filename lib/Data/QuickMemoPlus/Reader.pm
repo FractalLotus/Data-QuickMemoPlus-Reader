191be689b38debd8d2b6cd690ac572b43d8f6d52
Brent Shields <bshields@cpan.org>
2021-02-07 13:51:50 -0500

    updated pod

--- lib/Data/QuickMemoPlus/Reader.pm
+++ lib/Data/QuickMemoPlus/Reader.pm
@@ -141,7 +141,8 @@ Data::QuickMemoPlus::Reader - Extract text from QuickMemo+ LQM export files.
 =head1 DESCRIPTION
 
 C<Data::QuickMemoPlus::Reader> is a module that will extract the 
-text contents from archived QuickMemo+ memos.
+text contents from archived QuickMemo+ memos. QuickMemo+ is a memo 
+application that comes with LG smartphones.
 
 QuickMemo+ F<lqm> files are in Zip format. This module unzips them, 
 parses the json file inside, then extracts the category and memo text 
